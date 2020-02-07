aa6d665f2f76000873aa3d3a8e5cef731bb10a9c
Dimitrios Grammatikogiannis <dgrammatiko@gmail.com>
2020-02-07 16:10:32 +0100

    deploy fixes

---
+++ scripts/pre-deploy.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const fsExtra = require('fs-extra')
+const root = process.cwd();
+let combinedData = {};
+
+fsExtra.copy(`${root}/data`, `${root}/o_data`)
+    .then(() => {
+        fsExtra.removeSync(`${root}/data`);
+        fsExtra.mkdirSync(`${root}/data`);
+        const originalData = fs.readdirSync(`${root}/o_data`);
+
+        for (const theme of originalData) {
+            console.log(`${root}/o_data/${theme}`)
+            processTheme(`${root}/o_data/${theme}`);
+        }
+
+        return combinedData;
+    })
+    .then((combinedData) => {
+        fs.writeFileSync(`${root}/data/themes.json`, JSON.stringify(combinedData));
+        fsExtra.removeSync(`${root}/o_data`);
+    })
+    .catch(err => console.log(err));
+
+const processTheme = (jsonFile) => {
+    let sData;
+    if (fs.existsSync(jsonFile)) {
+        try {
+            sData = JSON.parse(fs.readFileSync(jsonFile))
+        } catch (er) {
+            throw new Error(er)
+        }
+    }
+
+    if (sData) {
+        combinedData = { ...combinedData, ...sData }
+    }
+};
