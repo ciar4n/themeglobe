b32f028a1d2f07fd89fc6699b34080ca52ab8268
dGrammatiko <d.grammatiko@gmail.com>
2020-02-17 20:01:17 +0100

    WIP Lighthouse data per theme (#11)

--- scripts/capture-lighthouse.js
+++ scripts/capture-lighthouse.js
@@ -1,26 +1,22 @@
 #!/usr/bin/env node
 
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const yamlFront = require('yaml-front-matter');
+const { post } = require('axios');
+const { existsSync, readFileSync, writeFileSync, mkdirSync, readdirSync } = require('fs');
+const { join } = require('path');
+const { loadFront } = require('yaml-front-matter');
 
-const themesFolder = path.join(__dirname, '../content/joomla');
-const themeFiles = fs.readdirSync(themesFolder);
-let lightHouseData = {};
+const themesFolder = join(__dirname, '../content/joomla');
+const themeFiles = readdirSync(themesFolder);
+const root = process.cwd();
 
-if (fs.existsSync(`${path.join(__dirname, '../data')}/themes.json`)) {
-  const tmpLhData = fs.readFileSync(`${path.join(__dirname, '../data')}/themes.json`);
-  try {
-    lightHouseData = JSON.parse(tmpLhData)
-  } catch (er) {
-    throw new Error(er)
-  }
+if (!existsSync(`${root}/data`)) {
+  mkdirSync(`${root}/data`);
 }
 
 const processTheme = (theme) => {
-  const dataTmp = fs.readFileSync(path.join(themesFolder, theme));
-  const frontmatter = yamlFront.loadFront(dataTmp);
+  const dataTmp = readFileSync(join(themesFolder, theme));
+  const frontmatter = loadFront(dataTmp);
+  const dataFile = `data/${theme.replace('.md', '').replace(/\-/g, '_')}.json`
   const data = {
     theme: theme,
     frontmatter: frontmatter
@@ -31,65 +27,84 @@ const processTheme = (theme) => {
     return;
   }
 
-  lh(data);
+  lh(data, dataFile);
 };
 
-const lh = async (data) => {
+const lh = async (data, dataFile) => {
+  let lightHouseData = {};
   let templateName = data.frontmatter.title;
   let provider = data.frontmatter.provider;
   let themeKey = `${provider}-${templateName}`.replace(/\s+/g, '-').toLowerCase();
-  const url = `${data.frontmatter.audit}?nocache=true`;
+  let url = '';
+
+  if (data.frontmatter.demo) {
+    url = data.frontmatter.demo
+  }
+  if (data.frontmatter.audit) {
+    url = data.frontmatter.audit
+  }
+
+  if (url === '') return;
+
+  url += '?nocache=true'
+
+  if (existsSync(dataFile)) {
+    try {
+      lightHouseData = JSON.parse(readFileSync(dataFile))
+    } catch (er) {
+      throw new Error(er)
+    }
+  }
 
   if (lightHouseData[`${themeKey}`]) {
     console.log(`${data.theme} Lighthouse skipped, already processed`)
     return;
   }
 
-  const d = JSON.stringify({
+  const themeData = JSON.stringify({
     url: url,
     replace: true,
     save: false
   });
 
-  const o = {
+  const opts = {
     headers: {
       'Content-Type': 'application/json',
     }
   };
 
-  axios.post('https://lighthouse-dot-webdotdevsite.appspot.com//lh/newaudit', d, o)
-    .then(function(response) {
+  post('https://lighthouse-dot-webdotdevsite.appspot.com//lh/newaudit', themeData, opts)
+    .then(function (response) {
       // handle success
       if (response.status === 200 || response.status === 201) {
         if (response.data && response.data.lhr) {
+          const lightHouseData = {};
           const out = response.data.lhr;
 
-          if (!lightHouseData.hasOwnProperty([`${themeKey}`])) {
-            carbonVal = out.audits['resource-summary'].details.items[0].size / 1024 / 1024 / 1024 * 0.06 * 1000
-            lightHouseData[`${themeKey}`] = {
-              performance: Math.ceil(out.categories.performance.score * 100),
-              firstContentfulPaint: Math.ceil(out.audits.metrics.details.items[0].firstContentfulPaint / 100) / 10,
-              firstMeaningfulPaint: Math.ceil(out.audits.metrics.details.items[0].firstMeaningfulPaint / 100) / 10,
-              firstCPUIdle: Math.ceil(out.audits.metrics.details.items[0].firstCPUIdle / 100) / 10,
-              interactive: Math.ceil(out.audits.metrics.details.items[0].interactive / 100) / 10,
-              bestPractices: Math.ceil(out.categories['best-practices'].score * 100),
-              accessibility: Math.ceil(out.categories.accessibility.score * 100),
-              seo: Math.ceil(out.categories.seo.score * 100),
-              carbon: carbonVal.toFixed(3),
-            };
+          carbonVal = out.audits['resource-summary'].details.items[0].size / 1024 / 1024 / 1024 * 0.06 * 1000
+          lightHouseData[`${themeKey}`] = {
+            performance: Math.ceil(out.categories.performance.score * 100),
+            firstContentfulPaint: Math.ceil(out.audits.metrics.details.items[0].firstContentfulPaint / 100) / 10,
+            firstMeaningfulPaint: Math.ceil(out.audits.metrics.details.items[0].firstMeaningfulPaint / 100) / 10,
+            firstCPUIdle: Math.ceil(out.audits.metrics.details.items[0].firstCPUIdle / 100) / 10,
+            interactive: Math.ceil(out.audits.metrics.details.items[0].interactive / 100) / 10,
+            bestPractices: Math.ceil(out.categories['best-practices'].score * 100),
+            accessibility: Math.ceil(out.categories.accessibility.score * 100),
+            seo: Math.ceil(out.categories.seo.score * 100),
+            carbon: carbonVal.toFixed(3),
           }
 
-          fs.writeFileSync(`${path.join(__dirname, '../data')}/themes.json`, JSON.stringify(lightHouseData));
-
+          writeFileSync(dataFile, JSON.stringify(lightHouseData));
         }
       }
 
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // handle error
-      console.log(error);
+      console.log(themeData);
+      console.dir(error);
     })
-    .then(function() {
+    .then(function () {
       // always executed
     });
 };
---
+++ scripts/pre-deploy.js
@@ -0,0 +1,35 @@
+const { existsSync, move, mkdirSync, readFileSync, readdirSync, writeFileSync } = require('fs-extra');
+const root = process.cwd();
+let combinedData = {};
+
+move(`${root}/data`, `${root}/o_data`)
+    .then(() => {
+        mkdirSync(`${root}/data`);
+        const originalData = readdirSync(`${root}/o_data`);
+
+        for (const theme of originalData) {
+            console.log(`${root}/o_data/${theme}`)
+            processTheme(`${root}/o_data/${theme}`);
+        }
+
+        return combinedData;
+    })
+    .then((combinedData) => {
+        writeFileSync(`${root}/data/themes.json`, JSON.stringify(combinedData));
+    })
+    .catch(err => console.log(err));
+
+const processTheme = (jsonFile) => {
+    let sData;
+    if (existsSync(jsonFile)) {
+        try {
+            sData = JSON.parse(readFileSync(jsonFile))
+        } catch (er) {
+            throw new Error(er)
+        }
+    }
+
+    if (sData) {
+        combinedData = { ...combinedData, ...sData }
+    }
+};
